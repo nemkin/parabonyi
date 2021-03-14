6604de297e166b9954e0f36be22b25c75bed94a7
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-14 16:01:04 +0100

    Fix graph generator

--- 2021_1_spring/algbony/parabonyi/helper/circle_coords.py
+++ 2021_1_spring/algbony/parabonyi/helper/circle_coords.py
@@ -4,31 +4,41 @@ from scipy.sparse import rand
 def to_char(num):
   return chr(ord('A') + num)
 
-n = 6
+def generate_graph(n, o_x, o_y, density):
+  lines = []
+  lines += ['\\begin{center}']
+  lines += ['\\begin{tikzpicture}[scale=2]']
+
+  for i in range(n):
+    angle = (math.pi * 2 / n) * i
+    c = to_char(i)
+    x = o_x + math.cos(angle)
+    y = o_y + math.sin(angle)
+    print(i,angle,x,y)
+    lines += [f'\coordinate ({c}) at ({x:.5f},{y:.5f});']
+
+  edges = rand(n, n, density=density)
+
+  for i,j in zip(edges.row, edges.col):
+    lines += [f'\draw[thick] ({to_char(i)}) -- ({to_char(j)});']
+
+  for i in range(n):
+    lines += [f'\draw[thick, fill=white] ({to_char(i)}) circle (0.1) node {{{to_char(i)}}};']
+
+  lines += ['\\end{tikzpicture}']
+  lines += ['\\end{center}']
+
+  return [line+'\n' for line in lines]
+
+n = 24
 o_x = 1
 o_y = 1
+ritka = generate_graph(n,o_x,o_y,0.02)
+suru = generate_graph(n,o_x,o_y,0.15)
 
+with open('../01_motivacio/ritka_graf.tex', 'w') as f:
+  f.writelines(ritka)
 
-
-print('\\begin{center}')
-print('\\begin{tikzpicture}[scale=2]')
-
-for i in range(n):
-  angle = 360 // n * i 
-  c = to_char(i)
-  x = o_x + math.cos(angle)
-  y = o_y + math.sin(angle)
-  print(f'\coordinate ({c}) at ({x:.2f},{y:.2f});')
-
-
-edges = rand(n, n, density=0.5)
-
-for i,j in zip(edges.row, edges.col):
-  print(f'\draw[thick] ({to_char(i)}) -- ({to_char(j)});');
-
-for i in range(n):
-  print(f'\draw[thick, fill=white] ({to_char(i)}) circle (0.2) node {to_char(i)};')
-
-print('\\end{tikzpicture}')
-print('\\end{center}')
+with open('../01_motivacio/suru_graf.tex', 'w') as f:
+  f.writelines(suru)
 
